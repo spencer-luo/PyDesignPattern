1a7dd2d87e9aa9f988a74354f4f4a3e335656625
Spencer.Luo <luoweifu12@gmail.com>
2018-05-20 17:47:40 +0800

    Add Terminal program

---
+++ application/TerminalMonitor.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+# Authoer: Spencer.Luo
+# Date: 5/20/2018
+
+# 引入升级版备忘录模式关键类
+from pattern.Memento import Originator, Caretaker, Memento
+
+class TerminalCmd(Originator):
+    "终端命令"
+
+    def __init__(self, text):
+        self.__cmdName = ""
+        self.__cmdArgs = []
+        self.parseCmd(text)
+
+    def parseCmd(self, text):
+        "从字符串中解析命令"
+        subStrs = self.getArgumentsFromString(text, " ")
+        # 获取第一个字段作为命令名称
+        if(len(subStrs) > 0):
+            self.__cmdName = subStrs[0]
+
+        # 获取第一个字段之后的所有字符作为命令的参数
+        if (len(subStrs) > 1):
+            self.__cmdArgs = subStrs[1:]
+
+    def getArgumentsFromString(self, str, splitFlag):
+        "通过splitFlag进行分割，获得参数数组."
+
+        if (splitFlag == ""):
+            print("splitFlag is empty!")
+            return ""
+
+        data = str.split(splitFlag)
+        result = []
+        for item in data:
+            item.strip()
+            if (item != ""):
+                result.append(item)
+
+        return result;
+
+    def showCmd(self):
+        print(self.__cmdName, self.__cmdArgs)
+
+class TerminalCaretaker(Caretaker):
+    "终端的备忘录管理类"
+
+    def showHistoryCmds(self):
+        "显示历史命令"
+        for key, obj in self._mementos.items():
+            name = ""
+            value = []
+            if(obj._TerminalCmd__cmdName):
+                name = obj._TerminalCmd__cmdName
+            if(obj._TerminalCmd__cmdArgs):
+                value = obj._TerminalCmd__cmdArgs
+            print("第" + str(key) + "条命令: " + str(name) + " " + str(value))
+
+
+def testTerminal():
+    cmdIdx = 0
+    caretaker = TerminalCaretaker()
+    curCmd = TerminalCmd("")
+    while (True):
+        strCmd = input("请输入指令：");
+        strCmd = strCmd.lower()
+        if (strCmd.startswith("q")):
+            exit(0)
+        elif(strCmd.startswith("h")):
+            caretaker.showHistoryCmds()
+        # 通过"!"符号表示获取历史的某个指令
+        elif(strCmd.startswith("!")):
+            idx = int(strCmd[1:])
+            curCmd.restoreFromMemento(caretaker.getMemento(idx))
+            curCmd.showCmd()
+        else:
+            curCmd = TerminalCmd(strCmd)
+            curCmd.showCmd()
+            caretaker.addMemento(cmdIdx, curCmd.createMemento())
+            cmdIdx +=1
+
+
+testTerminal()
--- pattern/Memento.py
+++ pattern/Memento.py
@@ -66,11 +66,46 @@ class TodoListCaretaker:
 #=======================================================================================================================
 # 代码框架
 #==============================
+from copy import deepcopy
+
+class Memento:
+    "备忘录"
+
+    def setAttribute(self, dict):
+        "深度拷贝字典dict中的所有属性"
+        self.__dict__ = deepcopy(dict)
+
+    def getAttribute(self):
+        "获取属性字典"
+        return self.__dict__
+
+
+class Caretaker:
+    "备忘录管理类"
+
+    def __init__(self):
+        self._mementos = {}
+
+    def addMemento(self, name, memento):
+        self._mementos[name] = memento
+
+    def getMemento(self, name):
+        return self._mementos[name]
+
+class Originator:
+    "备份发起人"
+
+    def createMemento(self):
+        memento = Memento()
+        memento.setAttribute(self.__dict__)
+        return memento
+
+    def restoreFromMemento(self, memento):
+        self.__dict__.update(memento.getAttribute())
 
 
 # 基于框架的实现
 #==============================
-
 
 # Test
 #=======================================================================================================================
@@ -91,6 +126,5 @@ def testEngineer():
     print()
     tony.retrospect(caretaker.getTodoList())
     tony.showWorkItem()
-
 
 testEngineer()
