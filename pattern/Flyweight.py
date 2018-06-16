143e0be864bb87536888bfa7578b4e0b32ff2391
Spencer.Luo <luoweifu12@gmail.com>
2018-06-16 14:41:27 +0800

    modify Flyweight

--- pattern/Flyweight.py
+++ pattern/Flyweight.py
@@ -21,22 +21,22 @@ class Pigment:
         return self
 
     def showInfo(self):
-        print(self.__user + str("取得") + self.__color + "色颜料")
+        print(self.__user + "取得" + self.__color + "色颜料")
 
 class PigmengFactory:
     "资料的工厂类"
 
     def __init__(self):
-        self.__sigmentPool = {
-            '红': Pigment('红'),
-            '黄': Pigment('黄'),
-            '蓝': Pigment('蓝'),
-            '绿': Pigment('绿'),
-            '紫': Pigment('紫'),
+        self.__sigmentSet = {
+            "红": Pigment("红"),
+            "黄": Pigment("黄"),
+            "蓝": Pigment("蓝"),
+            "绿": Pigment("绿"),
+            "紫": Pigment("紫"),
         }
 
     def getPigment(self, color):
-        pigment = self.__sigmentPool.get(color)
+        pigment = self.__sigmentSet.get(color)
         if pigment is None:
             logging.error("没有%s颜色的颜料！", color)
         return pigment
@@ -46,7 +46,36 @@ class PigmengFactory:
 #=======================================================================================================================
 # 代码框架
 #==============================
+from abc import ABCMeta, abstractmethod
+# 引入ABCMeta和abstractmethod来定义抽象类和抽象方法
 
+class Flyweight(metaclass=ABCMeta):
+    "享元类"
+
+    @abstractmethod
+    def operation(self, extrinsicState):
+        pass
+
+class FlyweightImpl(Flyweight):
+    "享元类的具体实现类"
+
+    def __init__(self, color):
+        self.__color = color
+
+    def operation(self, extrinsicState):
+        print(extrinsicState + "取得" + self.__color + "色颜料")
+
+class FlyweightFactory:
+    "享元工厂"
+
+    def __init__(self):
+        self.__flyweights = {}
+
+    def getFlyweight(self, key):
+        pigment = self.__flyweights.get(key)
+        if pigment is None:
+            pigment = FlyweightImpl(key)
+        return pigment
 
 # 基于框架的实现
 #==============================
@@ -56,17 +85,26 @@ class PigmengFactory:
 #=======================================================================================================================
 def testPigment():
     factory = PigmengFactory()
-    pigmentRed = factory.getPigment('红').setUser('梦之队')
+    pigmentRed = factory.getPigment("红").setUser("梦之队")
     pigmentRed.showInfo()
-    pigmentYellow = factory.getPigment('黄').setUser('梦之队')
+    pigmentYellow = factory.getPigment("黄").setUser("梦之队")
     pigmentYellow.showInfo()
-    pigmentBlue1 = factory.getPigment('蓝').setUser('梦之队')
+    pigmentBlue1 = factory.getPigment("蓝").setUser("梦之队")
     pigmentBlue1.showInfo()
-    pigmentBlue2 = factory.getPigment('蓝').setUser('和平队')
+    pigmentBlue2 = factory.getPigment("蓝").setUser("和平队")
     pigmentBlue2.showInfo()
 
 
-
+def testPigment2():
+    factory = FlyweightFactory()
+    pigmentRed = factory.getFlyweight("红")
+    pigmentRed.operation("梦之队")
+    pigmentYellow = factory.getFlyweight("黄")
+    pigmentYellow.operation("梦之队")
+    pigmentBlue1 = factory.getFlyweight("蓝")
+    pigmentBlue1.operation("梦之队")
+    pigmentBlue2 = factory.getFlyweight("蓝")
+    pigmentBlue2.operation("和平队")
 
 
 # print("Blue1:" + str(id(pigmentBlue1)) + ", Bule2:" + str(id(pigmentBlue2))
@@ -75,3 +113,4 @@ def testPigment():
 
 
 testPigment()
+# testPigment2()
