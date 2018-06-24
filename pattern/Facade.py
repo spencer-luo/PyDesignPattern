ec1a2dd02fcdfe95f1db09103f2dfcf432d0021a
Spencer.Luo <luoweifu12@gmail.com>
2018-06-24 16:19:59 +0800

    Modify Facade, add practical case

--- pattern/Facade.py
+++ pattern/Facade.py
@@ -59,6 +59,83 @@ class Volunteer:
 
 # 基于框架的实现
 #==============================
+from os import path
+import logging
+
+class ZIPModel:
+    "ZIP模块，负责ZIP文件的压缩与解压"
+
+    def compress(self, srcFilePath, dstFilePath):
+        print("ZIP模块正在进行 '" + srcFilePath + "' 文件的压缩......")
+        print("文件压缩成功，已保存至 '" + dstFilePath + "'")
+
+    def decompress(self, srcFilePath, dstFilePath):
+        print("ZIP模块正在进行 '" + srcFilePath + "' 文件的解压......")
+        print("文件解压成功，已保存至 '" + dstFilePath+ "'")
+
+
+class RARModel:
+    "RAR模块，负责ZIP文件的压缩与解压"
+
+    def compress(self, srcFilePath, dstFilePath):
+        print("RAR模块正在进行 '" + srcFilePath + "' 文件的压缩......")
+        print("文件压缩成功，已保存至 '" + dstFilePath + "'")
+
+    def decompress(self, srcFilePath, dstFilePath):
+        print("RAR模块正在进行 '" + srcFilePath + "' 文件的解压......")
+        print("文件解压成功，已保存至 '" + dstFilePath + "'")
+
+
+class ZModel:
+    "7Z模块，负责7Z文件的压缩与解压"
+
+    def compress(self, srcFilePath, dstFilePath):
+        print("7Z模块正在进行 '" + srcFilePath + "' 文件的压缩......")
+        print("文件压缩成功，已保存至 '" + dstFilePath + "'")
+
+    def decompress(self, srcFilePath, dstFilePath):
+        print("7Z模块正在进行 '" + srcFilePath + "' 文件的解压......")
+        print("文件解压成功，已保存至 '" + dstFilePath + "'")
+
+
+class CompressionFacade:
+    "压缩系统的外观类"
+
+    def __init__(self):
+        self.__zipModel = ZIPModel()
+        self.__rarModel = RARModel()
+        self.__zModel = ZModel()
+
+    def compress(self, srcFilePath, dstFilePath, type):
+        "根据不同的压缩类型，压缩成不同的格式"
+        # 获取新的文件名
+        extName = "." + type
+        fullName = dstFilePath + extName
+        if (type.lower() == "zip") :
+            self.__zipModel.compress(srcFilePath, fullName)
+        elif(type.lower() == "rar"):
+            self.__rarModel.compress(srcFilePath, fullName)
+        elif(type.lower() == "7z"):
+            self.__zModel.compress(srcFilePath, fullName)
+        else:
+            logging.error("Not support this format:" + str(type))
+            return False
+        return True
+
+    def decompress(self, srcFilePath, dstFilePath):
+        "从srcFilePath中获取后缀，根据不同的后缀名(拓展名)，进行不同格式的解压"
+        baseName = path.basename(srcFilePath)
+        extName = baseName.split(".")[1]
+        if (extName.lower() == "zip") :
+            self.__zipModel.decompress(srcFilePath, dstFilePath)
+        elif(extName.lower() == "rar"):
+            self.__rarModel.decompress(srcFilePath, dstFilePath)
+        elif(extName.lower() == "7z"):
+            self.__zModel.decompress(srcFilePath, dstFilePath)
+        else:
+            logging.error("Not support this format:" + str(extName))
+            return False
+        return True
 
 
 # Test
@@ -68,4 +145,36 @@ def testRegister():
     volunteer.welcomeFreshmen("Tony")
 
 
-testRegister()
+def testCompression():
+    facade = CompressionFacade()
+    facade.compress("E:\生活中的设计模式\生活中的外观模式——学妹别慌，学长帮你.md",
+                    "E:\压缩文件\生活中的外观模式——学妹别慌，学长帮你", "zip")
+    facade.decompress("E:\压缩文件\生活中的外观模式——学妹别慌，学长帮你.zip",
+                      "E:\解析文件\生活中的外观模式——学妹别慌，学长帮你.md")
+    print()
+
+    facade.compress("E:\生活中的设计模式\生活中的外观模式——学妹别慌，学长帮你.md",
+                    "E:\压缩文件\生活中的外观模式——学妹别慌，学长帮你", "rar")
+    facade.decompress("E:\压缩文件\生活中的外观模式——学妹别慌，学长帮你.rar",
+                      "E:\解析文件\生活中的外观模式——学妹别慌，学长帮你.md")
+    print()
+
+    facade.compress("E:\生活中的设计模式\生活中的外观模式——学妹别慌，学长帮你.md",
+                    "E:\压缩文件\生活中的外观模式——学妹别慌，学长帮你", "7z")
+    facade.decompress("E:\压缩文件\生活中的外观模式——学妹别慌，学长帮你.7z",
+                      "E:\解析文件\生活中的外观模式——学妹别慌，学长帮你.md")
+    print()
+
+
+def testPath():
+    filePath = "E:\解析文件\生活中的外观模式——学妹别慌，学长帮你.md"
+    dirName = path.dirname(filePath)
+    baseName = path.basename(filePath)
+    fileName, extName = baseName.split('.')
+    fullName = path.join(dirName, fileName + extName)
+    i = 0
+
+
+# testRegister()
+testCompression()
+# testPath()
