fd629ca5bde51e314fe493e8eb98301a82af9991
Spencer.Luo <luoweifu12@gmail.com>
2018-09-05 23:13:34 +0800

    Modify Observer.py

--- pattern/Observer.py
+++ pattern/Observer.py
@@ -115,6 +115,60 @@ class DrinkingMode(Observer):
             print("水已烧开！可以用来饮用了。")
 
 
+import time
+# 导入时间处理模块
+
+class Account(Observable):
+    """用户账户"""
+
+    def __init__(self):
+        super().__init__()
+        self.__latestIp = {}
+        self.__latestRegion = {}
+
+    def login(self, name, ip, time):
+        region = self.__getRegion(ip)
+        if self.__isLongDistance(name, region):
+            self.notifyObservers({"name": name, "ip": ip, "region": region, "time": time})
+        self.__latestRegion[name] = region
+        self.__latestIp[name] = ip
+
+    def __getRegion(self, ip):
+        # 由IP地址获取地区信息。这里只是模拟，真实项目中应该调用IP地址解析服务
+        ipRegions = {
+            "101.47.18.9": "浙江省杭州市",
+            "67.218.147.69":"美国洛杉矶"
+        }
+        region = ipRegions.get(ip)
+        return "" if region is None else region
+
+
+    def __isLongDistance(self, name, region):
+        # 计算本次登录与最近几次登录的地区差距。
+        # 这里只是简单地用字符串匹配来模拟，真实的项目中应该调用地理信息相关的服务
+        latestRegion = self.__latestRegion.get(name)
+        return latestRegion is not None and latestRegion != region;
+
+
+
+class SmsSender(Observer):
+    """短信发送器"""
+
+    def update(self, observable, object):
+        print("[短信发送] " + object["name"] + "您好！检测到您的账户可能登录异常。最近一次登录信息：\n"
+              + "登录地区：" + object["region"] + "  登录ip：" + object["ip"] + "  登录时间："
+              + time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(object["time"])))
+
+
+class MailSender(Observer):
+    """邮件发送器"""
+
+    def update(self, observable, object):
+        print("[邮件发送] " + object["name"] + "您好！检测到您的账户可能登录异常。最近一次登录信息：\n"
+              + "登录地区：" + object["region"] + "  登录ip：" + object["ip"] + "  登录时间："
+              + time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(object["time"])))
+
+
 def testWaterHeater():
     heater = WaterHeater()
     washingObser = WashingMode()
@@ -126,4 +180,27 @@ def testWaterHeater():
     heater.setTemperature(100)
 
 
-testWaterHeater()
+def testLogin():
+    accout = Account()
+    accout.addObserver(SmsSender())
+    accout.addObserver(MailSender())
+    accout.login("Tony", "101.47.18.9", time.time())
+    accout.login("Tony", "67.218.147.69", time.time())
+
+
+
+def testTime():
+    print(time.time())
+    strTime = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(time.time()))
+    print(strTime)
+
+# testWaterHeater()
+testLogin()
+# testTime()
+
+# ipRegion = {
+#             "101.47.18.9": "浙江省杭州市",
+#             "67.218.147.69":"美国洛杉矶"
+#         }
+#
+# print(ipRegion["101.47.18.90"])
