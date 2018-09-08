a80f3cf7c95c7d86a3084193374ba84518d5ea6d
Spencer.Luo <luoweifu12@gmail.com>
2018-09-08 17:22:00 +0800

    Modify Mediator.py

--- pattern/Mediator.py
+++ pattern/Mediator.py
@@ -5,14 +5,14 @@
 # Version 1.0
 #=======================================================================================================================
 class HouseInfo:
-    "房源信息"
+    """房源信息"""
 
-    def __init__(self, area, price, hasWindow, bathroom, kitchen, address, owner):
+    def __init__(self, area, price, hasWindow, hasBathroom, hasKitchen, address, owner):
         self.__area = area
         self.__price = price
-        self.__window = hasWindow
-        self.__bathroom = bathroom
-        self.__kitchen = kitchen
+        self.__hasWindow = hasWindow
+        self.__hasBathroom = hasBathroom
+        self.__hasKitchen = hasKitchen
         self.__address = address
         self.__owner = owner
 
@@ -25,16 +25,15 @@ class HouseInfo:
     def showInfo(self, isShowOwner = True):
         print("面积:" + str(self.__area) + "平米",
               "价格:" + str(self.__price) + "元",
-              "窗户:" + ("有" if self.__window else "没有"),
-              "卫生间:" + self.__bathroom,
-              "厨房:" + ("有" if self.__kitchen else "没有"),
-              "地址:" + self.getAddress(),
+              "窗户:" + ("有" if self.__hasWindow else "没有"),
+              "卫生间:" + self.__hasBathroom,
+              "厨房:" + ("有" if self.__hasKitchen else "没有"),
+              "地址:" + self.__address,
               "房东:" + self.getOwnerName() if isShowOwner else "")
 
 
-
 class HousingAgency:
-    "房屋中介"
+    """房屋中介"""
 
     def __init__(self, name):
         self.__houseInfos = []
@@ -52,42 +51,40 @@ class HousingAgency:
                 self.__houseInfos.remove(info)
 
     def getSearchCondition(self, description):
-        "这里有一个将用户描述信息转换成搜索条件的逻辑。(为节省篇幅这里原样返回描述)"
+        """这里有一个将用户描述信息转换成搜索条件的逻辑
+        (为节省篇幅这里原样返回描述)"""
         return description
 
     def getMatchInfos(self, searchCondition):
-        "根据房源信息的各个属性查找最匹配的信息。(为节省篇幅这里略去匹配的过程，全部输出)"
-        print(self.getName(), "为您找以下最适合的房源：")
+        """根据房源信息的各个属性查找最匹配的信息
+        (为节省篇幅这里略去匹配的过程，全部输出)"""
+        print(self.getName(), "为您找到以下最适合的房源：")
         for info in self.__houseInfos:
             info.showInfo(False)
         return  self.__houseInfos
 
-    def signContract(self, houseInfo, time):
-        "与房东签订协议"
+    def signContract(self, houseInfo, period):
+        """与房东签订协议"""
         print(self.getName(), "与房东", houseInfo.getOwnerName(), "签订", houseInfo.getAddress(),
-              "的房子的的租赁合同，租期", time, "年。 合同期内", self.getName(), "有权对其进行使用和转租！")
+              "的房子的的租赁合同，租期", period, "年。 合同期内", self.getName(), "有权对其进行使用和转租！")
 
-    def signContracts(self, time):
+    def signContracts(self, period):
         for info in self.__houseInfos :
-            self.signContract(info, time)
+            self.signContract(info, period)
 
 
 class HouseOwner:
-    "房东"
+    """房东"""
 
-    def __init__(self, name, address):
+    def __init__(self, name):
         self.__name = name
-        self.__address = address
         self.__houseInfo = None
 
     def getName(self):
         return self.__name
 
-    def getAddress(self):
-        return self.__address
-
-    def setHouseInfo(self, area, price, hasWindow, bathroom, kitchen):
-        self.__houseInfo = HouseInfo(area, price, hasWindow, bathroom, kitchen, self.getAddress(), self)
+    def setHouseInfo(self, address, area, price, hasWindow, bathroom, kitchen):
+        self.__houseInfo = HouseInfo(area, price, hasWindow, bathroom, kitchen, address, self)
 
     def publishHouseInfo(self, agency):
         agency.addHouseInfo(self.__houseInfo)
@@ -96,7 +93,7 @@ class HouseOwner:
 
 
 class Custom:
-    "用户，租房人"
+    """用户，租房的贫下中农"""
 
     def __init__(self, name):
         self.__name = name
@@ -110,39 +107,15 @@ class Custom:
         return agency.getMatchInfos(agency.getSearchCondition(description))
 
     def seeHouse(self, houseInfos):
-        "去看房，选择最使用的房子。(这里省略看房的过程)"
+        """去看房，选择最使用的房子
+        (这里省略看房的过程)"""
         size = len(houseInfos)
         return houseInfos[size-1]
 
-    def signContract(self, houseInfo, agency, time):
-        "与中介签订协议"
+    def signContract(self, houseInfo, agency, period):
+        """与中介签订协议"""
         print(self.getName(), "与中介", agency.getName(), "签订", houseInfo.getAddress(),
-              "的房子的租赁合同, 租期", time, "年。合同期内", self.__name, "有权对其进行使用！")
-
-
-# class ZhangSan(HouseOwner):
-#     "张三"
-#
-#     def __init__(self, name, address):
-#         super().__init__(name, address)
-#         self.houseInfo = HouseInfo(20, 2500, 1, "独立卫生间", 0, address, name)
-#
-# class LiSi(HouseOwner):
-#     "李四"
-#
-#     def __init__(self, name, address):
-#         super().__init__(name, address)
-#         self.houseInfo = HouseInfo(16, 1800, 1, "公用卫生间", 0, address, name)
-#
-#
-# class WangWu(HouseOwner):
-#     "王五"
-#
-#     def __init__(self, name, address):
-#         super().__init__(name, address)
-#         self.houseInfo = HouseInfo(18, 2600, 1, "独立卫生间", 1, address, name)
-
-# area, price, hasWindow, bathroom, kitchen, address, owner
+              "的房子的租赁合同, 租期", period, "年。合同期内", self.__name, "有权对其进行使用！")
 
 # Version 2.0
 #=======================================================================================================================
@@ -159,14 +132,14 @@ class Custom:
 
 def testRenting():
     myHome = HousingAgency("我爱我家")
-    zhangsan = HouseOwner("张三", "上地西里");
-    zhangsan.setHouseInfo(20, 2500, 1, "独立卫生间", 0)
+    zhangsan = HouseOwner("张三");
+    zhangsan.setHouseInfo("上地西里", 20, 2500, 1, "独立卫生间", 0)
     zhangsan.publishHouseInfo(myHome)
-    lisi = HouseOwner("李四", "当代城市家园")
-    lisi.setHouseInfo(16, 1800, 1, "公用卫生间", 0)
+    lisi = HouseOwner("李四")
+    lisi.setHouseInfo("当代城市家园", 16, 1800, 1, "公用卫生间", 0)
     lisi.publishHouseInfo(myHome)
-    wangwu = HouseOwner("王五", "金隅美和园")
-    wangwu.setHouseInfo(18, 2600, 1, "独立卫生间", 1)
+    wangwu = HouseOwner("王五")
+    wangwu.setHouseInfo("金隅美和园", 18, 2600, 1, "独立卫生间", 1)
     wangwu.publishHouseInfo(myHome)
     print()
 
