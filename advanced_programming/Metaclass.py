8d946c5c98c674f8f9ded6cd65a5c52bd33b5df8
Spencer.Luo <luoweifu12@gmail.com>
2018-09-17 22:24:52 +0800

    Add Metaclass.py

--- advanced_programming/Metaclass.py
+++ advanced_programming/Metaclass.py
@@ -95,9 +95,8 @@ class CustomMetaclass(type):
         print("CustomMetaclass.__init__ cls:", cls)
         super().__init__(what, bases, dict)
 
-
     def __call__(cls, *args, **kwargs):
-        print("CustomMetaclass.__call__ args:", args)
+        print("CustomMetaclass.__call__ args:", args, kwargs)
         self = super(CustomMetaclass, cls).__call__(*args, **kwargs)
         print("CustomMetaclass.__call__ self:", self)
         return self
@@ -108,7 +107,7 @@ class CustomClass(metaclass=CustomMetaclass):
         print("CustomClass.__init__ self:", self)
         super().__init__()
 
-    def __new__(cls, *args):
+    def __new__(cls, *args, **kwargs):
         self = super().__new__(cls)
         print("CustomClass.__new__, self:", self)
         return self
@@ -116,10 +115,9 @@ class CustomClass(metaclass=CustomMetaclass):
     def __call__(self, *args, **kwargs):
         print("CustomClass.__call__ args:", args)
 
-obj = CustomClass()
+obj = CustomClass("Meta arg1", "Meta arg2", kwarg1=1, kwarg2=2)
 print(type(CustomClass))
 print(obj)
-CustomClass("Meta arg1", "Meta arg2")
 obj("arg1", "arg2")
 
 
