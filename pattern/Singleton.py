8bc4f8ed3a8fe217c8cf4e1f0b7761b97a942c20
Spencer.Luo <luoweifu12@gmail.com>
2018-09-18 22:13:54 +0800

    Add Singleton.py

--- pattern/Singleton.py
+++ pattern/Singleton.py
@@ -55,29 +55,34 @@ class MyBeautifulGril(object):
 
 # 方式二
 #========================================================================================
-# class Singleton2(type):
-#     def __init__(cls, name, bases, attrs):
-#         super(Singleton2, cls).__init__(name, bases, attrs)
-#         cls._instance = None
-#
-#     def __call__(cls, *args, **kwargs):
-#         if not cls._instance:
-#             cls._instance = super(Singleton2, cls).__call__(*args, **kwargs)
-#         return cls._instance
-#
-# class MyClass(metaclass=Singleton2):
-#     pass
-#     def __init__(self, name):
-#         self.__name = name
-#
-#     def getName(self):
-#         return self.__name
-#
-# s0 = MyClass("Zhangsan")
-# s1 = MyClass("Lisi")
-# print(s0.getName(), s1.getName())
-# print("id(s0):", id(s0), "id(s1):", id(s1))
-# print("s0 == s1:", s0 == s1)
+class Singleton2(type):
+    """单例实现方式二"""
+
+    def __init__(cls, what, bases=None, dict=None):
+        super().__init__(what, bases, dict)
+        cls._instance = None # 初始化全局变量cls._instance为None
+
+    def __call__(cls, *args, **kwargs):
+        # 控制对象的创建过程，如果cls._instance为None则创建，否则直接返回
+        if cls._instance is None:
+            cls._instance = super().__call__(*args, **kwargs)
+        return cls._instance
+
+class CustomClass(metaclass=Singleton2):
+    """用户自定义的类"""
+
+    def __init__(self, name):
+        self.__name = name
+
+    def getName(self):
+        return self.__name
+
+
+tony = CustomClass("Tony")
+karry = CustomClass("Karry")
+print(tony.getName(), karry.getName())
+print("id(tony):", id(tony), "id(karry):", id(karry))
+print("tony == karry:", tony == karry)
 
 
 # 单例的装饰器
