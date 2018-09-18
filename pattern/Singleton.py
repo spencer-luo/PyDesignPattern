8ef9a0c306bc64ad7abb1461ea8bdb71bc82365a
Spencer.Luo <luoweifu12@gmail.com>
2018-09-18 22:45:27 +0800

    Modify Singleton.py

--- pattern/Singleton.py
+++ pattern/Singleton.py
@@ -55,21 +55,53 @@ class MyBeautifulGril(object):
 
 # 方式二
 #========================================================================================
-class Singleton2(type):
-    """单例实现方式二"""
+# class Singleton2(type):
+#     """单例实现方式二"""
+#
+#     def __init__(cls, what, bases=None, dict=None):
+#         super().__init__(what, bases, dict)
+#         cls._instance = None # 初始化全局变量cls._instance为None
+#
+#     def __call__(cls, *args, **kwargs):
+#         # 控制对象的创建过程，如果cls._instance为None则创建，否则直接返回
+#         if cls._instance is None:
+#             cls._instance = super().__call__(*args, **kwargs)
+#         return cls._instance
+#
+# class CustomClass(metaclass=Singleton2):
+#     """用户自定义的类"""
+#
+#     def __init__(self, name):
+#         self.__name = name
+#
+#     def getName(self):
+#         return self.__name
+#
+#
+# tony = CustomClass("Tony")
+# karry = CustomClass("Karry")
+# print(tony.getName(), karry.getName())
+# print("id(tony):", id(tony), "id(karry):", id(karry))
+# print("tony == karry:", tony == karry)
 
-    def __init__(cls, what, bases=None, dict=None):
-        super().__init__(what, bases, dict)
-        cls._instance = None # 初始化全局变量cls._instance为None
 
-    def __call__(cls, *args, **kwargs):
-        # 控制对象的创建过程，如果cls._instance为None则创建，否则直接返回
-        if cls._instance is None:
-            cls._instance = super().__call__(*args, **kwargs)
-        return cls._instance
+# 方式三
+#========================================================================================
+def singletonDecorator(cls, *args, **kwargs):
+    """定义一个单例装饰器"""
+    instance = {}
 
-class CustomClass(metaclass=Singleton2):
-    """用户自定义的类"""
+    def wrapperSingleton(*args, **kwargs):
+        if cls not in instance:
+            instance[cls] = cls(*args, **kwargs)
+        return instance[cls]
+
+    return wrapperSingleton
+
+
+@singletonDecorator
+class Singleton3:
+    """使用单例装饰器修饰一个类"""
 
     def __init__(self, name):
         self.__name = name
@@ -78,51 +110,29 @@ class CustomClass(metaclass=Singleton2):
         return self.__name
 
 
-tony = CustomClass("Tony")
-karry = CustomClass("Karry")
-print(tony.getName(), karry.getName())
-print("id(tony):", id(tony), "id(karry):", id(karry))
-print("tony == karry:", tony == karry)
-
-
-# 单例的装饰器
-def singletonDecorator(cls, *args, **kwargs):
-    "构造一个单例的装饰器"
-    instance = {}
-
-    def __singleton(*args, **kwargs):
-        if cls not in instance:
-            instance[cls] = cls(*args, **kwargs)
-        return instance[cls]
-
-    return __singleton
-
-# @singletonDecorator
-# class Singleton3:
-#     "使用修饰器修饰的类为单例类"
-#     def __init__(self, name):
-#         self.__name = name
-#
-#     def getName(self):
-#         return self.__name
-#
-# s0 = Singleton3("Zhangsan")
-# s1 = Singleton3("Lisi")
-# print(s0.getName(), s1.getName())
-# print("id(s0):", id(s0), "id(s1):", id(s1))
-# print("s0 == s1:", s0 == s1)
+# tony = Singleton3("Tony")
+# karry = Singleton3("Karry")
+# print(tony.getName(), karry.getName())
+# print("id(tony):", id(tony), "id(karry):", id(karry))
+# print("tony == karry:", tony == karry)
 
 
 # Version 2.0
 ########################################################################################################################
-# @singletonDecorator
-# class Love:
-#     "真爱"
-#     def __init__(self, name):
-#         self.__name = name
-#
-#     def showMyHeart(self):
-#         print("I love", self.__name, "forever!")
+@singletonDecorator
+class MyBeautifulGril(object):
+    """我的漂亮女神"""
+
+    def __init__(self, name):
+        self.__name = name
+        if self.__name == name:
+            print("遇见" + name + "，我一见钟情！")
+        else:
+            print("遇见" + name + "，我置若罔闻！")
+
+    def showMyHeart(self):
+        print(self.__name + "就我心中的唯一！")
+
 
 # Test
 ########################################################################################################################
@@ -134,5 +144,5 @@ def TestLove():
     print("id(jenny):", id(jenny), " id(kimi):", id(kimi))
 
 
-# TestLove()
+TestLove()
 
