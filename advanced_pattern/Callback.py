1e245194560a3e94273161a62ad825a05e7f78f7
Spencer.Luo <luoweifu12@gmail.com>
2018-11-04 16:37:05 +0800

    Modify Callback.py

--- advanced_pattern/Callback.py
+++ advanced_pattern/Callback.py
@@ -39,7 +39,78 @@ def skateboarding():
 
 # Version 2.0
 #=======================================================================================================================
-# 代码框架
+# 代码框架1：面向过程的实现方式
+#==============================
+def callback(*args, **kwargs):
+    """回调函数"""
+    # todo 函数体的实现
+
+
+def otherFun(fun, *args, **kwargs):
+    """高阶函数，也叫包含函数"""
+    # todo 函数体的实现
+
+# 函数的调用方式
+otherFun(callable)
+
+
+# 代码框架2：面向对象的实现方式
+#==============================
+from abc import ABCMeta, abstractmethod
+# 引入ABCMeta和abstractmethod来定义抽象类和抽象方法
+
+class Strategy(metaclass=ABCMeta):
+    """算法的抽象类"""
+
+    @abstractmethod
+    def algorithm(self, *args, **kwargs):
+        """定义算法"""
+        pass
+
+class StrategyA(Strategy):
+    """策略A"""
+
+    def algorithm(self, *args, **kwargs):
+        print("算法A的实现...")
+
+class StrategyB(Strategy):
+    """策略B"""
+
+    def algorithm(self, *args, **kwargs):
+        print("算法B的实现...")
+
+class Context:
+    """上下文环境类"""
+
+    def interface(self, strategy, *args, **kwargs):
+        """交互接口"""
+        print("回调执行前的操作")
+        strategy.algorithm()
+        print("回调执行后的操作")
+
+# # 调用方式
+# context = Context()
+# context.interface(StrategyA())
+# context.interface(StrategyB())
+
+
+# 基于框架的实现1
+#==============================
+def isEvenNumber(num):
+    return num % 2 == 0
+
+def isGreaterThanTen(num):
+    return num > 10
+
+def getResultNumbers(fun, elements):
+    newList = []
+    for item in elements:
+        if (fun(item)):
+            newList.append(item)
+    return newList
+
+
+# 基于框架的实现2
 #==============================
 from abc import ABCMeta, abstractmethod
 # 引入ABCMeta和abstractmethod来定义抽象类和抽象方法
@@ -88,9 +159,6 @@ class Skateboarding(Skill):
         print("酷炫滑板")
 
 
-# 基于框架的实现
-#==============================
-
 # Test
 #=======================================================================================================================
 
@@ -119,29 +187,21 @@ def testStrategySkill():
     Kerry.doPerformance(Skateboarding())
 
 
-def isEvenNumber(num):
-    return num % 2 == 0
-
-def isGreaterThanTen(num):
-    return num > 10
-
-def getEvenNumbers(fun, elements):
-    newList = []
-    for item in elements:
-        if (fun(item)):
-            newList.append(item)
-    return newList
-
 def testCallback():
     elements = [2, 3, 6, 9, 12, 15, 18]
-    # list1 = getEvenNumbers(isEvenNumber, elements)
-    # list2 = getEvenNumbers(isGreaterThanTen, elements)
+    list1 = getResultNumbers(isEvenNumber, elements)
+    list2 = getResultNumbers(isGreaterThanTen, elements)
+    print("所有的偶数：", list1)
+    print("大于10的数：", list2)
+
+def testFilter():
+    elements = [2, 3, 6, 9, 12, 15, 18]
     list1 = list(filter(lambda x: x % 2 == 0, elements))
     list2 = list(filter(lambda x: x > 10, elements))
     print("所有的偶数：", list1)
     print("大于10的数：", list2)
 
-
 # testSkill()
 testStrategySkill()
 # testCallback()
+# testFilter()
